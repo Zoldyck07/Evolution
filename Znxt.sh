94c9df07a39dbfab2a0fc595c1cf0b477359820c
Zoldyck07 <zoldyck07@ymail.com>
2014-05-10 14:54:50 +0800

    Added Znxt build script

---
+++ Znxt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TOOLCHAIN="/home/zoldyck/android/4.3/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
+MODULES_DIR="/home/zoldyck/android/4.3/modules"
+KERNEL_DIR="/home/zoldyck/android/4.3"
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- Znxt_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j2
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+echo "Copying modules"
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$TOOLCHAIN-strip --strip-unneeded *.ko
+cd $KERNEL_DIR
+else
+echo "Dafuq its failed! Fix the errors!"
+fi
